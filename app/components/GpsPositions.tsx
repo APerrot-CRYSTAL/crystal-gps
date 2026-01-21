7cfb6802fb923a7463cf05b33b91e211045cc0c5
Cyphle <cyril.phamle@gmail.com>
2026-01-21 19:56:08 +0100

    position table

--- app/components/GpsPositions.tsx
+++ app/components/GpsPositions.tsx
@@ -1,29 +1,47 @@
 'use client';
 
 import { GpsPosition } from '@/app/domain/gps/position';
-import { Stack, Table, TableBody, TableCell, TableHead } from '@mui/material';
+import { IconButton, Stack, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export interface GpsPositionsProps {
   positions: GpsPosition[];
+  onEdit: (id: string) => void;
+  onDelete: (id: string) => void;
 }
 
 export default function GpsPositions(props: GpsPositionsProps) {
-  const { positions } = props;
+  const { positions, onEdit, onDelete } = props;
 
   return (
-      <Stack spacing={2}>
+      <Stack spacing={ 2 }>
         <Table>
           <TableHead>
-            <TableCell sx={{ fontWeight: 800 }}>Name</TableCell>
-            <TableCell sx={{ fontWeight: 800 }}>Longitude</TableCell>
-            <TableCell sx={{ fontWeight: 800 }}>Latitude</TableCell>
-            <TableCell sx={{ fontWeight: 800 }} align="right">Action</TableCell>
+            <TableRow>
+              <TableCell sx={ { fontWeight: 800 } }>Name</TableCell>
+              <TableCell sx={ { fontWeight: 800 } }>Longitude</TableCell>
+              <TableCell sx={ { fontWeight: 800 } }>Latitude</TableCell>
+              <TableCell sx={ { fontWeight: 800 } } align="right">Action</TableCell>
+            </TableRow>
           </TableHead>
 
           <TableBody>
-            {/*{positions.map(position => (*/}
-            {/*    */}
-            {/*))}*/}
+            { positions.map(position => (
+                <TableRow key={ position.id }>
+                  <TableCell>{ position.name }</TableCell>
+                  <TableCell>{ position.longitude }</TableCell>
+                  <TableCell>{ position.latitude }</TableCell>
+                  <TableCell>
+                    <IconButton aria-label={ `edit-${ position.name }` } onClick={ () => onEdit(position.id) }>
+                      <EditIcon fontSize="small"/>
+                    </IconButton>
+                    <IconButton aria-label={ `delete-${ position.name }` } onClick={ () => onDelete(position.id) }>
+                      <DeleteIcon fontSize="small"/>
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+            )) }
           </TableBody>
         </Table>
       </Stack>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+'use client';
+
 import { Container, Stack } from '@mui/material';
 import GpsPositions from '@/app/components/GpsPositions';
 import { GpsPosition } from '@/app/domain/gps/position';
@@ -6,10 +7,18 @@ import { GpsPosition } from '@/app/domain/gps/position';
 const positions: GpsPosition[] = [];
 
 export default function Home() {
+  const onEdit = (id: string) => {}
+
+  const onDelete = (id: string) => {}
+
   return (
     <Container maxWidth="md" sx={{ py: 4 }}>
       <Stack spacing={2}>
-        <GpsPositions positions={positions}></GpsPositions>
+        <GpsPositions
+            positions={positions}
+            onEdit={onEdit}
+            onDelete={onDelete}
+        ></GpsPositions>
       </Stack>
     </Container>
   );
